ea9f56e3f86d4b12e2e4f1a28fe86ea3e61f612d
Grandparez <notmyvegetable@gmail.com>
2025-03-09 20:38:11 +0800

    Initial setup, restyling the ui

--- generalpurposeapplication.client/src/app/app.module.ts
+++ generalpurposeapplication.client/src/app/app.module.ts
@@ -4,14 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    FetchDataComponent,
+    HealthCheckComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
