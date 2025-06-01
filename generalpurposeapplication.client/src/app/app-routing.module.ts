e466960bd70bb51a02b50709b423e1e7df3a7cdf
Grandparez <notmyvegetable@gmail.com>
2025-06-01 14:00:01 +0800

    Added authguard

--- generalpurposeapplication.client/src/app/app-routing.module.ts
+++ generalpurposeapplication.client/src/app/app-routing.module.ts
@@ -9,16 +9,18 @@ import { CategoriesComponent } from './categories/categories.component';
 import { CategoryEditComponent } from './categories/category-edit.component';
 import { LoginComponent } from './auth/login.component';
 
+import { AuthGuard } from './auth/auth.guard';
+
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'fetch-data', component: FetchDataComponent },
   { path: 'health-check', component: HealthCheckComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'product/:id', component: ProductEditComponent },
-  { path: 'product', component: ProductEditComponent },
+  { path: 'product/:id', component: ProductEditComponent, canActivate: [AuthGuard] },
+  { path: 'product', component: ProductEditComponent, canActivate: [AuthGuard] },
   { path: 'categories', component: CategoriesComponent },
-  { path: 'category/:id', component: CategoryEditComponent },
-  { path: 'category', component: CategoryEditComponent },
+  { path: 'category/:id', component: CategoryEditComponent, canActivate: [AuthGuard] },
+  { path: 'category', component: CategoryEditComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
