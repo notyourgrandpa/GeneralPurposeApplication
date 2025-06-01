6309b888ecd077c6ae0fbaaa4b7890fc9f69f622
Grandparez <notmyvegetable@gmail.com>
2025-06-01 13:42:51 +0800

    Added authinterceptors

--- generalpurposeapplication.client/src/app/app.module.ts
+++ generalpurposeapplication.client/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth/auth.interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -40,7 +41,11 @@ import { LoginComponent } from './auth/login.component';
     AngularMaterialModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
