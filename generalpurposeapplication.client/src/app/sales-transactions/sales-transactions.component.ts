4dab8419cbe725ec2d7ad247fe8780a296bd3396
Piolo <notmyvegetable@gmail.com>
2025-09-02 15:33:36 +0800

    Added sales-transactions OnInit implementation

--- generalpurposeapplication.client/src/app/sales-transactions/sales-transactions.component.ts
+++ generalpurposeapplication.client/src/app/sales-transactions/sales-transactions.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sales-transactions',
   templateUrl: './sales-transactions.component.html',
   styleUrl: './sales-transactions.component.scss'
 })
-export class SalesTransactionsComponent {
+export class SalesTransactionsComponent implements OnInit {
+  ngOnInit(): void {
+      throw new Error('Method not implemented.');
+  }
+  public displayedColumns: string[] = [
+    'id',
+    'n'
+  ]
 
 }
