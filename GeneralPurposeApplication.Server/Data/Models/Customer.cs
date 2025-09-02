fcffad7f9992346e6833cea43dcef22e3d709759
Piolo <notmyvegetable@gmail.com>
2025-09-02 15:33:06 +0800

    Added customers

---
+++ GeneralPurposeApplication.Server/Data/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeneralPurposeApplication.Server.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? ContactNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
