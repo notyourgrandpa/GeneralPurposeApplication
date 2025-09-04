8db42d4be6ff01039511b733e87d3dd12b89927f
Piolo <notmyvegetable@gmail.com>
2025-09-04 14:42:21 +0800

    Added SalesTransactionDTO

---
+++ GeneralPurposeApplication.Server/Data/DTOs/SalesTransactionCreateInputDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneralPurposeApplication.Server.Data.DTOs
+{
+    public class SalesTransactionCreateInputDTO
+    {
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string? CustomerId { get; set; } // optional if you support customers
+        public List<SalesTransactionItemCreateInputDTO> Items { get; set; } = new();
+
+    }
+}
---
+++ GeneralPurposeApplication.Server/Data/DTOs/SalesTransactionsDTO.cs
@@ -0,0 +1,15 @@
+﻿using GeneralPurposeApplication.Server.Data.Models;
+
+namespace GeneralPurposeApplication.Server.Data.DTOs
+{
+    public class SalesTransactionsDTO
+    {
+        public int Id { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int Quantity { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public string ProcessedByUserId { get; set; } = null!;
+        public string ProcessedByUserName { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
