80cda11f934994f37823bdac118074d4636ed105
Piolo <notmyvegetable@gmail.com>
2025-10-02 08:01:45 +0800

    Moved customer to folder-based structure

--- generalpurposeapplication.client/src/app/customers/services/customer.service.ts
+++ generalpurposeapplication.client/src/app/customers/services/customer.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Customer } from './customer';
+import { Customer } from '../models/customer';
 import { Observable } from 'rxjs';
 
 @Injectable({
