921c33549fcab92fdedbd04f27f9b97b67e7e810
Grandparez <notmyvegetable@gmail.com>
2025-10-05 21:12:20 +0800

    Added paid-enough validator

---
+++ generalpurposeapplication.client/src/app/shared/validators/paid-enough.validator.ts
@@ -0,0 +1,19 @@
+import { AbstractControl, FormArray, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export const paidEnoughValidator: ValidatorFn = (form: AbstractControl): ValidationErrors | null =>{
+  const items = form.get('items') as FormArray;
+  const paidAmount = form.get('paidAmount')?.value ?? 0;
+
+  if (!items || !items.length) return null; // No items yet — don’t validate
+  
+  // Compute total based on items
+  const total = items.controls.reduce((sum, item) => {
+    const qty = item.get('qty')?.value || 0;
+    const price = item.get('price')?.value || 0;
+    return sum + (qty * price);
+  }, 0);
+
+  console.log(items);
+
+  return paidAmount >= total ? null : { insufficientPayment: true}
+}
