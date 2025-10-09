93f3c108b820d9185bd9c677e77545a1d4892dd6
Piolo <notmyvegetable@gmail.com>
2025-10-09 11:23:18 +0800

    Added IInventoryLogRepository

---
+++ GeneralPurposeApplication.Server/Repositories/IInventoryLogRepository.cs
@@ -0,0 +1,19 @@
+﻿using GeneralPurposeApplication.Server.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Repositories
+{
+    public interface IInventoryLogRepository
+    {
+        public Task<InventoryLog?> GetInventoryLogById(int id);
+        public Task<IEnumerable<InventoryLog>> GetAllInventoryLogsAsync();
+        public Task AddInventoryLogAsync(InventoryLog log);
+        public Task AddInventoryLogsAsync(IEnumerable<InventoryLog> logs);
+        public Task UpdateInventoryLogAsync(InventoryLog log);
+        public Task DeleteInventoryLogAsync(int id);
+    }
+}
