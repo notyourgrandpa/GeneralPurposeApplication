385379994ca051606d8d357df37d9d6411ef5c4a
Piolo <notmyvegetable@gmail.com>
2025-10-10 10:02:39 +0800

    Added IRepository for tracking changes

---
+++ GeneralPurposeApplication.Server/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using GeneralPurposeApplication.Server.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context) => _context = context;
+
+        public async Task<T?> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().AsNoTracking().ToListAsync();
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+        public Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            return Task.CompletedTask;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            
+            if(entity != null)
+                _context.Set<T>().Remove(entity);
+        }
+    }
+}
