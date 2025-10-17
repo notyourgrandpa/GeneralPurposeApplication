96f1d727eb952064791404a64d9edc298e3e0c40
Grandparez <notmyvegetable@gmail.com>
2025-10-17 20:27:44 +0800

    Renamed to CategoryUpdateDTO

---
+++ GeneralPurposeApplication.Server/Data/DTOs/CategoryUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Data.DTOs
+{
+    public class CategoryUpdateDTO: CategoryCreateInputDTO
+    {
+        public int Id { get; set; }
+    }
+}
