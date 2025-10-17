740cf006c17f9a74553fa5cac162619f8bc286fa
Grandparez <notmyvegetable@gmail.com>
2025-10-17 20:29:16 +0800

    Added ICategoryService

---
+++ GeneralPurposeApplication.Server/Services/ICategoryService.cs
@@ -0,0 +1,27 @@
+﻿using GeneralPurposeApplication.Server.Data;
+using GeneralPurposeApplication.Server.Data.DTOs;
+using GeneralPurposeApplication.Server.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Services
+{
+    public interface ICategoryService
+    {
+        Task<ApiResult<CategoryDTO>> GetCategoriesAsync(
+            int pageIndex,
+            int pageSize,
+            string? sortColumn,
+            string? sortOrder,
+            string? filterColumn,
+            string? filterQuery);
+
+        Task<Category?> GetCategoryAsync(int id);
+        Task<Category> CreateCategoryAsync(CategoryCreateInputDTO categoryCreateDTO);
+        Task UpdateCategoryAsync(int id, CategoryUpdateDTO categoryUpdateDTO);
+        Task<bool> DeleteCategoryAsync(int id);
+    }
+}
