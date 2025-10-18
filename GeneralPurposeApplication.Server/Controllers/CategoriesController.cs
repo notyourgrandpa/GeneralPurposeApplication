fab3c5fda04990010cb4b7608c3d0537797694a4
Grandparez <notmyvegetable@gmail.com>
2025-10-18 19:55:08 +0800

    Refactored deleteCategory

--- GeneralPurposeApplication.Server/Controllers/CategoriesController.cs
+++ GeneralPurposeApplication.Server/Controllers/CategoriesController.cs
@@ -99,18 +99,9 @@ namespace GeneralPurposeApplication.Server.Controllers
         // DELETE: api/Categories/5
         [Authorize(Roles = "Administrator")]
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCategory(int id)
+        public async Task<bool> DeleteCategory(int id)
         {
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return await _categoryService.DeleteCategoryAsync(id);
         }
 
         private bool CategoryExists(int id)
