7b544c75b9c434de95fdcf352634bb34016f3822
Grandparez <notmyvegetable@gmail.com>
2025-10-19 21:41:11 +0800

    Renamed ProductUpdateInputDTO to ProductUpdateDTO

--- GeneralPurposeApplication.Server/Data/DTOs/ProductUpdateDTO.cs
+++ GeneralPurposeApplication.Server/Data/DTOs/ProductUpdateDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GeneralPurposeApplication.Server.Data.DTOs
 {
-    public class ProductUpdateInputDTO: ProductCreateInputDTO
+    public class ProductUpdateDTO: ProductCreateDTO
     {
         public int Id { get; set; }
     }
