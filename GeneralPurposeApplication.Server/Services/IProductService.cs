a28607db178ddc38a5879ffa808c16c55cad91c0
Grandparez <notmyvegetable@gmail.com>
2025-10-19 21:41:29 +0800

    Added CRUD methods

--- GeneralPurposeApplication.Server/Services/IProductService.cs
+++ GeneralPurposeApplication.Server/Services/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using GeneralPurposeApplication.Server.Data;
+using GeneralPurposeApplication.Server.Data.DTOs;
 using GeneralPurposeApplication.Server.Data.Models;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,10 @@ namespace GeneralPurposeApplication.Server.Services
 {
     public interface IProductService
     {
+        Task<ApiResult<ProductDTO>> GetProduct(int id);
+        Task<Product> GetProductAsync(int productId);
+        Task UpdateProductAsync(int productId, ProductUpdateDTO productUpdateDTO);
+        Task<bool> DeleteProductAsync(int productId);
         Task UpdateStockAsync(InventoryLog inventoryLog);
     }
 }
