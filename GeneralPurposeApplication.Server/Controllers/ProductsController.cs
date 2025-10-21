3f119b75da63bfded421cb84279592097c7c15be
Piolo <notmyvegetable@gmail.com>
2025-10-21 09:05:25 +0800

    Renamed GetProduct to GetProductAsync

--- GeneralPurposeApplication.Server/Controllers/ProductsController.cs
+++ GeneralPurposeApplication.Server/Controllers/ProductsController.cs
@@ -44,7 +44,7 @@ namespace GeneralPurposeApplication.Server.Controllers
 
         // GET: api/Products/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProductAsync(int id)
         {
             var product = await _context.Products.FindAsync(id);
 
