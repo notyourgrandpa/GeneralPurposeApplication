4635a07b896af291b7133fe625ea065be5b26f34
Grandparez <notmyvegetable@gmail.com>
2025-10-25 18:25:26 +0800

    Added ISalesTransactionService

---
+++ GeneralPurposeApplication.Server/Services/ISalesTransactionService.cs
@@ -0,0 +1,20 @@
+﻿using GeneralPurposeApplication.Server.Data;
+using GeneralPurposeApplication.Server.Data.DTOs;
+using GeneralPurposeApplication.Server.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Services
+{
+    public interface ISalesTransactionService
+    {
+        Task<ApiResult<SalesTransactionsDTO>> GetSalesTransactionsAsync();
+        Task<SalesTransaction?> GetSalesTransactionAsync();
+        Task<SalesTransactionsDTO> CreateSalesTransactionAsync();
+        Task<bool> UpdateSalesTransactionAsync();
+        Task<bool> DeleteSalesTransactionAsync();
+    }
+}
