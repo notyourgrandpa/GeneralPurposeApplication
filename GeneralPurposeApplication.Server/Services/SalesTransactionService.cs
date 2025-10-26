e0c510a484cdb6959ecdabfdb970bd722c931396
Grandparez <notmyvegetable@gmail.com>
2025-10-26 20:46:52 +0800

    Added paramters to SalesTransactionsAsync

--- GeneralPurposeApplication.Server/Services/SalesTransactionService.cs
+++ GeneralPurposeApplication.Server/Services/SalesTransactionService.cs
@@ -19,7 +19,7 @@ namespace GeneralPurposeApplication.Server.Services
             _unitOfWork = unitOfWork;
         }
 
-        public Task<ApiResult<SalesTransactionsDTO>> GetSalesTransactionsAsync()
+        public Task<ApiResult<SalesTransactionsDTO>> GetSalesTransactionsAsync(int pageIndex, int pageSize, string? sortColumn, string? sortOrder, string? filterColumn, string? filterQuery)
         {
             throw new NotImplementedException();
         }
