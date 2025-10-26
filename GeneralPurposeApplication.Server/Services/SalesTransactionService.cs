a3cff44a380f4e498f7833b3d52fd199c49fc1ba
Grandparez <notmyvegetable@gmail.com>
2025-10-26 20:36:14 +0800

    Inherited ISalesTransactionService

---
+++ GeneralPurposeApplication.Server/Services/SalesTransactionService.cs
@@ -0,0 +1,42 @@
+﻿using GeneralPurposeApplication.Server.Data;
+using GeneralPurposeApplication.Server.Data.DTOs;
+using GeneralPurposeApplication.Server.Data.Models;
+using GeneralPurposeApplication.Server.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Services
+{
+    public class SalesTransactionService : ISalesTransactionService
+    {
+
+        public Task<ApiResult<SalesTransactionsDTO>> GetSalesTransactionsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SalesTransaction?> GetSalesTransactionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SalesTransactionsDTO> CreateSalesTransactionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateSalesTransactionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<bool> DeleteSalesTransactionAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
