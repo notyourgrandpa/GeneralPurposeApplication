cbb862bf5fac7f97cbd4df9c66570188695bd48a
Grandparez <notmyvegetable@gmail.com>
2025-11-08 16:53:01 +0800

    refactor(sales transaction): remove try catch

--- GeneralPurposeApplication.Server/Controllers/SalesTransactionsController.cs
+++ GeneralPurposeApplication.Server/Controllers/SalesTransactionsController.cs
@@ -68,17 +68,11 @@ namespace GeneralPurposeApplication.Server.Controllers
         [HttpPost]
         public async Task<ActionResult<SalesTransaction>> CreateSalesTransactionAsync(SalesTransactionCreateDTO salesTransactionLogDto)
         {
-            try
-            {
-                var salesTransactionDto = await _salesTransactionService.CreateSalesTransactionAsync(salesTransactionLogDto, User.GetUserId());
+            var salesTransactionDto = await _salesTransactionService.CreateSalesTransactionAsync(salesTransactionLogDto, User.GetUserId());
 
-                return CreatedAtAction("GetSalesTransaction", new { id = salesTransactionDto.Id }, salesTransactionDto);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-    }
+            return CreatedAtAction("GetSalesTransaction", new { id = salesTransactionDto.Id }, salesTransactionDto);
+            return BadRequest(new { message = ex.Message });
+        }
 
 
         [HttpDelete("{id}")]
