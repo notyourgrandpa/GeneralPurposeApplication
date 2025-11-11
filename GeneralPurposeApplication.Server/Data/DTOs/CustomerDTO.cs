482e7a7aed8460f94508b277f6547c8348a582f4
Piolo <notmyvegetable@gmail.com>
2025-11-11 13:05:08 +0800

    refactor(customer): add nullable contact number

--- GeneralPurposeApplication.Server/Data/DTOs/CustomerDTO.cs
+++ GeneralPurposeApplication.Server/Data/DTOs/CustomerDTO.cs
@@ -10,6 +10,6 @@ namespace GeneralPurposeApplication.Server.Data.DTOs
     {
         public int Id { get; set; }
         public string Name { get; set;  } = string.Empty;
-        public string ContactNumber { get; set; } = string.Empty;
+        public string? ContactNumber { get; set; } = string.Empty;
     }
 }
