35623905f6edf0bfc90317542f90f6d20f3442b2
Piolo <notmyvegetable@gmail.com>
2025-11-17 14:41:32 +0800

    feat(seed): inherit ISeedService

---
+++ GeneralPurposeApplication.Server/Services/SeedService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Services
+{
+    public class SeedService : ISeedController
+    {
+        public Task CreateDefaultUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Import()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
