45d2a9c8983777317c9a05f90e63a7d553a3f5aa
Piolo <notmyvegetable@gmail.com>
2025-11-18 13:52:47 +0800

    refactor(seed): add SeedResultDTO return type for Import

--- GeneralPurposeApplication.Server/Services/ISeedService.cs
+++ GeneralPurposeApplication.Server/Services/ISeedService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeneralPurposeApplication.Server.Data.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace GeneralPurposeApplication.Server.Services
 {
     public interface ISeedService
     {
-        Task Import();
+        Task<SeedResultDTO> Import();
         Task CreateDefaultUser();
     }
 }
