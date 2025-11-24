e60d83ca7e73ff28994132b9b25f56fa5abc1829
Grandparez <notmyvegetable@gmail.com>
2025-11-24 17:21:22 +0800

    feat(query parameter): add query parameter

---
+++ GeneralPurposeApplication.Server/Data/QueryParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Data
+{
+    public class QueryParameter
+    {
+        public int pageIndex = 0;
+        public int pageSize = 10;
+        public string? sortColumn = null;
+        public string? sortOrder = null;
+        public string? filterColumn = null;
+        public string? filterQuery = null;
+
+    }
+}
