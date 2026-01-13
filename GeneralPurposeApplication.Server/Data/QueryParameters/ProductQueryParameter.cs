10f2ff50d69a45b92d3d45f2f5b27cc749764b12
Piolo Macayan <notmyvegetable@gmail.com>
2026-01-13 11:02:40 +0800

    feat(product): add productQueryParameter

---
+++ GeneralPurposeApplication.Server/Data/QueryParameters/ProductQueryParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Server.Data.QueryParameters
+{
+    public class ProductQueryParameter: QueryParameter
+    {
+        public int? categoryId { get; set; }
+    }
+}
