7e5df5589c3908d87b00b2ef3abc8e5335427188
Piolo Macayan <notmyvegetable@gmail.com>
2026-02-12 10:52:02 +0800

    fix(tailwind): fix tailwind not working - fix tailwind conflict with outlines

--- generalpurposeapplication.client/tailwind.config.js
+++ generalpurposeapplication.client/tailwind.config.js
@@ -1,7 +1,6 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
