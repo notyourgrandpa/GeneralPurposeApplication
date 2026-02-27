d3cae185aa1a4fb35b0e654a5919c7706075361a
Piolo Macayan <notmyvegetable@gmail.com>
2026-02-27 23:37:15 +0800

    feat(gpa): Add IApplicaitonDbContext

---
+++ GeneralPurposeApplication.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GeneralPurposeApplication.Domain.Categories;
+using GeneralPurposeApplication.Domain.Inventory;
+using GeneralPurposeApplication.Domain.Products;
+using GeneralPurposeApplication.Domain.Sales;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<SalesTransaction> SalesTransactions { get; set; }
+        DbSet<InventoryLog> InventoryLogs { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
