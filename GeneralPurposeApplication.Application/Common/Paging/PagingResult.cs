3aa1e2e9ef8227a1b6df7f221af2175fb2cb097b
Piolo Macayan <notmyvegetable@gmail.com>
2026-03-06 14:45:00 +0800

    feat(paging): add interface for a dynamic apiResult refactor

---
+++ GeneralPurposeApplication.Application/Common/Paging/PagingResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Common.Paging
+{
+    public class PagingResult<T>
+    {
+        public List<T> Data { get; set; } = new();
+        public int PageIndex { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages { get; private set; }
+ 
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 0);
+            }
+        }
+        public bool HasNextPage
+        {
+            get
+            {
+                return ((PageIndex + 1) < TotalPages);
+            }
+        }
+        public string? SortColumn { get; set; }
+        public string? SortOrder { get; set; }
+        public string? FilterColumn { get; set; }
+        public string? FilterQuery { get; set; }
+    }
+}
