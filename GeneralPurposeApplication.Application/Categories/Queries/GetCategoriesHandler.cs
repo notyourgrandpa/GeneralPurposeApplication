5885a88d377bc2a0c6b11eda8891b27abf4cf925
Piolo Macayan <notmyvegetable@gmail.com>
2026-03-06 14:41:27 +0800

    feat(category): add cqs for categories

---
+++ GeneralPurposeApplication.Application/Categories/Queries/GetCategoriesHandler.cs
@@ -0,0 +1,37 @@
+﻿using GeneralPurposeApplication.Application.Common.Interfaces;
+using GeneralPurposeApplication.Application.Common.Paging;
+using GeneralPurposeApplication.Application.DTOs;
+using GeneralPurposeApplication.Application.QueryParameters;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Categories.Queries
+{
+    public class GetCategoriesHandler
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetCategoriesHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ApiResult<CategoryDTO>> Handle(QueryParameter parameters)
+        {
+            return await ApiResult<CategoryDTO>.CreateAsync(
+                 _context.Categories
+                .AsNoTracking()
+                .Select(c => new CategoryDTO
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    TotalProducts = c.Products.Count(),
+                }), parameters);
+
+        }
+    }
+}
