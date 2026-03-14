d638c4a111933629446ec29414be65a0da8629fb
Piolo Macayan <notmyvegetable@gmail.com>
2026-03-14 19:56:00 +0800

    feat(query): add apply filter

--- GeneralPurposeApplication.Infrastructure/Persistence/QueryExecutor.cs
+++ GeneralPurposeApplication.Infrastructure/Persistence/QueryExecutor.cs
@@ -1,10 +1,12 @@
 ﻿using GeneralPurposeApplication.Application.Common.Interfaces;
 using GeneralPurposeApplication.Application.Common.Paging;
 using GeneralPurposeApplication.Application.QueryParameters;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,8 +23,48 @@ namespace GeneralPurposeApplication.Infrastructure.Persistence
 
         public Task<PagingResult<TDto>> ExecuteAsync<TEntity,TDto>(PagingQuery query) where TEntity : class
         {
-            IQueryable<TEntity> queryable = _context.Set<TEntity>();
+            IQueryable<TEntity> source = _context.Set<TEntity>().AsNoTracking();
+
+            source = ApplyFilters(source, query.Filters ?? new List<FilterCondition>());
+
+
             throw new NotImplementedException();
+        }
+
+        private IQueryable<TEntity> ApplyFilters<TEntity>(IQueryable<TEntity> source, List<FilterCondition> filters)
+        {
+            var param = Expression.Parameter(typeof(TEntity), "x");
+
+            Expression ? combinedExpression = null;
+            foreach (FilterCondition filter in filters) 
+            { 
+                Expression property = Expression.PropertyOrField(param, filter.Field);
+                Expression constant = Expression.Constant(Convert.ChangeType(filter.Value, property.Type));
+
+                Expression? condition = filter.Operator switch
+                {
+                    FilterOperator.Contains => Expression.Call(property, typeof(string).GetMethod("Contains", new[] { typeof(string) })!, constant),
+                    FilterOperator.StartsWith => Expression.Call(property, typeof(string).GetMethod("StartsWith", new[] { typeof(string) })!, constant),
+                    FilterOperator.Equals => Expression.Equal(property, constant),
+                    FilterOperator.GreaterThan => Expression.GreaterThan(property, constant),
+                    FilterOperator.LessThan => Expression.LessThan(property, constant),
+                    _ => null,
+                };
+
+                if (condition != null)
+                {
+                    combinedExpression = combinedExpression == null ? condition : Expression.AndAlso(combinedExpression, condition);
+                }
+            }
+
+            if(combinedExpression == null)
+            {
+                return source;
+            }
+
+            var lambda = Expression.Lambda<Func<TEntity, bool>>(combinedExpression, param);
+
+            return source.Where(lambda);
         }
     }
 }
