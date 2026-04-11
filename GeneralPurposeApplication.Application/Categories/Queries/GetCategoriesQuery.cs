04284f0d8cc26f3c7b8031d506eb5b5f04ce88ea
Piolo Macayan <notmyvegetable@gmail.com>
2026-04-11 14:15:33 +0800

    refactor(category): refactor GetCategories

---
+++ GeneralPurposeApplication.Application/Categories/Queries/GetCategoriesQuery.cs
@@ -0,0 +1,12 @@
+﻿using GeneralPurposeApplication.Application.Common.Paging;
+using GeneralPurposeApplication.Application.DTOs;
+using GeneralPurposeApplication.Application.QueryParameters;
+using MediatR;
+
+namespace GeneralPurposeApplication.Application.Categories.Queries
+{
+    public class GetCategoriesQuery: IRequest<PagingResult<CategoryDTO>>
+    {
+        public PagingQuery Query { get; set; } = new();
+    }
+}
