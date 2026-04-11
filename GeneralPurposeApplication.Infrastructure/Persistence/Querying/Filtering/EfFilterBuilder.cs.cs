d8a75112c18ea72f72f5498277d7be4d8aa56ffe
Piolo Macayan <notmyvegetable@gmail.com>
2026-04-11 14:14:57 +0800

    refactor(query): refactor QueryExecutor - add BuilderHlpers

---
+++ GeneralPurposeApplication.Infrastructure/Persistence/Querying/Filtering/EfFilterBuilder.cs.cs
@@ -0,0 +1,49 @@
+﻿using GeneralPurposeApplication.Application.QueryParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Infrastructure.Persistence.Querying.Filtering
+{
+    public class EfFilterBuilder
+    {
+        public IQueryable<TEntity> Apply<TEntity>(IQueryable<TEntity> source, List<FilterCondition> filters) where TEntity : class
+        {
+            var param = Expression.Parameter(typeof(TEntity), "x");
+
+            Expression? combinedExpression = null;
+            foreach (FilterCondition filter in filters)
+            {
+                Expression property = Expression.PropertyOrField(param, filter.Field);
+                Expression constant = Expression.Constant(Convert.ChangeType(filter.Value, property.Type));
+
+                Expression? condition = filter.Operator switch
+                {
+                    FilterOperator.Contains => Expression.Call(property, typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) })!, constant),
+                    FilterOperator.StartsWith => Expression.Call(property, typeof(string).GetMethod(nameof(string.StartsWith), new[] { typeof(string) })!, constant),
+                    FilterOperator.Equals => Expression.Equal(property, constant),
+                    FilterOperator.GreaterThan => Expression.GreaterThan(property, constant),
+                    FilterOperator.LessThan => Expression.LessThan(property, constant),
+                    _ => null,
+                };
+
+                if (condition != null)
+                {
+                    combinedExpression = combinedExpression == null ? condition : Expression.AndAlso(combinedExpression, condition);
+                }
+            }
+
+            if (combinedExpression == null)
+            {
+                return source;
+            }
+
+            var lambda = Expression.Lambda<Func<TEntity, bool>>(combinedExpression, param);
+
+            return source.Where(lambda);
+        }
+    }
+}
---
+++ GeneralPurposeApplication.Infrastructure/Persistence/Querying/Sorting/EfSortBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Infrastructure.Persistence.Querying.Sorting
+{
+    public class EfSortBuilder
+    {
+        public IQueryable<TEntity> Apply<TEntity>(IQueryable<TEntity> source, string? sortColumn, string? sortDirection) where TEntity : class
+        {
+            if (sortColumn == null)
+                return source;
+            var param = Expression.Parameter(typeof(TEntity), "x");
+
+            Expression property = Expression.PropertyOrField(param, sortColumn);
+
+            var lambda = Expression.Lambda(property, param);
+
+            string methodName =
+                sortDirection?.ToUpper() == "DESC"
+                ? "OrderByDescending"
+                : "OrderBy";
+
+            var resultExpression = Expression.Call(
+                typeof(Queryable),
+                methodName,
+                new Type[] { typeof(TEntity), property.Type },
+                source.Expression,
+                Expression.Quote(lambda));
+
+            return source.Provider.CreateQuery<TEntity>(resultExpression);
+        }
+    }
+}
