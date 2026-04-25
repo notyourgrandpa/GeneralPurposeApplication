74b7152b22657c339bbf6469b48047b8b0783d27
Piolo Macayan <notmyvegetable@gmail.com>
2026-04-25 23:59:20 +0800

    refactor(category): change putCategoryAsync with cqrs

---
+++ GeneralPurposeApplication.Application/Categories/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Categories.Commands
+{
+    public class UpdateCategoryCommand: IRequest<Unit>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
