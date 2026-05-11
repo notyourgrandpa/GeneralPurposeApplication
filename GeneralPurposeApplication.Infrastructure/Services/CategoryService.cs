a7077c85432b274c209a865b2d72384bcda050db
Piolo Macayan <notmyvegetable@gmail.com>
2026-05-11 20:48:22 +0800

    change(category): change IsDupeField to using dbcontext

--- GeneralPurposeApplication.Infrastructure/Services/CategoryService.cs
+++ GeneralPurposeApplication.Infrastructure/Services/CategoryService.cs
@@ -1,24 +1,29 @@
-﻿using GeneralPurposeApplication.Application.Common.Paging;
+﻿using GeneralPurposeApplication.Application.Common.Interfaces;
+using GeneralPurposeApplication.Application.Common.Paging;
 using GeneralPurposeApplication.Application.DTOs;
 using GeneralPurposeApplication.Application.Services;
 using GeneralPurposeApplication.Domain.Abstractions;
 using GeneralPurposeApplication.Domain.Categories;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Text;
 using System.Threading.Tasks;
+using static FluentValidation.Validators.PredicateValidator;
+using static OfficeOpenXml.ExcelErrorValue;
 
 namespace GeneralPurposeApplication.Infrastructure.Services
 {
     public class CategoryService: ICategoryService
     {
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly IApplicationDbContext _context;
 
-        public CategoryService(IUnitOfWork unitOfWork) 
+        public CategoryService(IApplicationDbContext context) 
         { 
-            _unitOfWork = unitOfWork;
+            _context = context;
         }
 
         public async Task<bool> IsDupeField(int categoryId, string fieldName, string fieldValue)
@@ -27,10 +32,11 @@ namespace GeneralPurposeApplication.Infrastructure.Services
             if (!ApiResult<Category>.IsValidProperty(fieldName, true))
                 return false;
 
-            return await _unitOfWork.Repository<Category>().AnyAsync(
-                string.Format("{0} == @0 && Id != @1", fieldName),
-                fieldValue,
-                categoryId);
+            string predicate = string.Format("{0} == @0 && Id != @1", fieldName);
+
+            object[] values = { fieldValue, categoryId };
+
+            return await _context.Set<T>().Where(predicate, values).AnyAsync();
         }
     }
 }
