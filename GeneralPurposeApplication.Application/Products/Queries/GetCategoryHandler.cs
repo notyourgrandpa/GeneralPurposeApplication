8b740f017cddabc63d6f9f0d94875dc12160ab71
Piolo Macayan <notmyvegetable@gmail.com>
2026-05-17 20:56:49 +0800

    change(product): add cqrs of getProduct

---
+++ GeneralPurposeApplication.Application/Products/Queries/GetCategoryHandler.cs
@@ -0,0 +1,34 @@
+﻿using GeneralPurposeApplication.Application.Common.Interfaces;
+using GeneralPurposeApplication.Domain.Products;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Products.Queries
+{
+    public class GetCategoryHandler : IRequestHandler<GetProductQuery, Product>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetCategoryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> Handle(GetProductQuery request, CancellationToken cancellationToken = default)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if(product == null)
+            {
+                throw new KeyNotFoundException($"Product {request.Id} not found!");
+            }
+            return product;
+        }
+    }
+}
