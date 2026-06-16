d8291ad3386f2be2f7fcfdcf41e47908a368cf27
Piolo Macayan <notmyvegetable@gmail.com>
2026-06-16 11:14:58 +0800

    change(gpa): add refacto audit log migrations

---
+++ GeneralPurposeApplication.Infrastructure/Persistence/Migrations/20260616031354_RefactorAuditLogs.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneralPurposeApplication.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorAuditLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EntityKey",
+                table: "AuditLogs",
+                type: "varchar(500)",
+                unicode: false,
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EntityKey",
+                table: "AuditLogs");
+        }
+    }
+}
