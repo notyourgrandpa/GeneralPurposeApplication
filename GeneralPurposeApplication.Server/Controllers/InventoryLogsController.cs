25f630ed37152257af1f6c7d9705a53d7abe1bdf
Piolo Macayan <notmyvegetable@gmail.com>
2026-06-20 20:40:07 +0800

    refactor(inventory log): change voiding to VoidInventoryLogCommand

--- GeneralPurposeApplication.Server/Controllers/InventoryLogsController.cs
+++ GeneralPurposeApplication.Server/Controllers/InventoryLogsController.cs
@@ -1,9 +1,11 @@
 ﻿using GeneralPurposeApplication.Application.Common.Paging;
 using GeneralPurposeApplication.Application.DTOs;
+using GeneralPurposeApplication.Application.Products.Commands;
 using GeneralPurposeApplication.Application.Services;
 using GeneralPurposeApplication.Application.UseCases;
 using GeneralPurposeApplication.Domain.Inventory;
 using GeneralPurposeApplication.Infrastructure.Persistence.Extensions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -20,11 +22,13 @@ namespace GeneralPurposeApplication.Server.Controllers
     {
         private readonly IInventoryLogService _inventoryLogService;
         private readonly AddStockUseCase _addStockUseCase;
+        private readonly IMediator _mediator;
 
-        public InventoryLogsController(IInventoryLogService inventoryLogService, AddStockUseCase addStockUseCase)
+        public InventoryLogsController(IInventoryLogService inventoryLogService, AddStockUseCase addStockUseCase, IMediator mediator)
         {
             _inventoryLogService = inventoryLogService;
             _addStockUseCase = addStockUseCase;
+            _mediator = mediator;
         }
         // GET: api/InventoryLogs
         // GET: api/InventoryLogs/?pageIndex=0&pageSize=10
@@ -65,7 +69,7 @@ namespace GeneralPurposeApplication.Server.Controllers
         [HttpPatch("{id}")]
         public async Task<IActionResult> VoidInventoryLogAsync(int id)
         {
-            await _inventoryLogService.VoidInventoryLogAsync(id,User.GetUserId());
+            await _mediator.Send(new VoidInventoryLogCommand { Id = id, UserId = User.GetUserId() });
             return NoContent();
         }
     }
