90a09abb664be07010fcb5663156503d9806dd55
Piolo Macayan <notmyvegetable@gmail.com>
2026-06-20 20:36:31 +0800

    change(inventory log): add invnetoryLogCommand

---
+++ GeneralPurposeApplication.Application/Inventory Logs/Commands/VoidInventoryLogHandler.cs
@@ -0,0 +1,78 @@
+﻿using GeneralPurposeApplication.Application.Common.Interfaces;
+using GeneralPurposeApplication.Application.Products.Commands;
+using GeneralPurposeApplication.Domain.Abstractions;
+using GeneralPurposeApplication.Domain.Inventory;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Inventory_Logs.Commands
+{
+    public class VoidInventoryLogHandler : IRequestHandler<VoidInventoryLogCommand, Unit>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public VoidInventoryLogHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(VoidInventoryLogCommand request, CancellationToken cancellationToken)
+        {
+
+            var inventoryLog = await _context.InventoryLogs.FindAsync(request.Id);
+
+            if (inventoryLog == null)
+                throw new KeyNotFoundException($"Inventory Log {request.Id} not found.");
+
+            if (inventoryLog.IsVoided)
+                throw new InvalidOperationException("This inventory log is already voided.");
+
+            // Void the log
+            inventoryLog.IsVoided = true;
+            inventoryLog.VoidedAt = DateTime.UtcNow;
+            inventoryLog.VoidedByUserId = request.UserId;
+
+            var logs = _context.InventoryLogs
+                .Where(l => l.ProductId == inventoryLog.ProductId && l.Id != request.Id)
+                .OrderBy(l => l.Date)
+                .ThenBy(l => l.Id);
+
+            int runningStock = 0;
+
+            foreach (var log in logs)
+            {
+                if (log.IsVoided)
+                    continue;
+
+                log.OldStock = runningStock;
+
+                switch (log.ChangeType)
+                {
+                    case InventoryChangeType.StockIn:
+                        runningStock += log.Quantity;
+                        break;
+
+                    case InventoryChangeType.StockOut:
+                        runningStock -= log.Quantity;
+                        break;
+
+                    case InventoryChangeType.Adjustment:
+                        runningStock = log.Quantity; // absolute adjustment
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Unknown inventory change type.");
+                }
+            }
+
+            inventoryLog.Product!.Stock = runningStock;
+
+            await _context.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+}
