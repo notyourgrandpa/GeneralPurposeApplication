0f49174e3b0289b5b92f6bf945c6f8c4895e1a4a
Piolo Macayan <notmyvegetable@gmail.com>
2026-06-27 13:26:03 +0800

    change(inventory log): add GetInventoryLogQuery

---
+++ GeneralPurposeApplication.Application/Inventory Logs/Queries/GetInventoryLogQuery.cs
@@ -0,0 +1,15 @@
+﻿using GeneralPurposeApplication.Domain.Inventory;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralPurposeApplication.Application.Inventory_Logs.Queries
+{
+    public class GetInventoryLogQuery : IRequest<InventoryLog>
+    {
+        public int InventoryLogId { get; set;}
+    }
+}
