64d8757670675c7b287aca2a82b3142817fc16e6
Piolo Macayan <notmyvegetable@gmail.com>
2026-08-25 20:33:42 +0800

    change(category): add closing of categoryEditDialog and snackbar

--- generalpurposeapplication.client/src/app/categories/components/category-edit/category-edit.component.ts
+++ generalpurposeapplication.client/src/app/categories/components/category-edit/category-edit.component.ts
@@ -29,12 +29,12 @@ export class CategoryEditComponent extends BaseFormComponent implements OnInit{
   categories?: Category[];
   constructor(
     private fb: FormBuilder,
-    private activatedRoute: ActivatedRoute,
     private router: Router,
     private categoryService: CategoryService,
     private categoryGraphQlService: CategoryGraphQlService,
     @Inject(MAT_DIALOG_DATA) public data: { id: number },
-    private dialogRef: MatDialogRef<CategoryEditComponent>
+    private dialogRef: MatDialogRef<CategoryEditComponent>,
+    private snackBar: MatSnackBar
   ) {
     super();
   }
@@ -78,8 +78,8 @@ export class CategoryEditComponent extends BaseFormComponent implements OnInit{
           .subscribe({
             next: (result) => {
               console.log("Category " + category!.id + " has been updated.");
-              // go back to categories view
-              this.router.navigate(['/categories']);
+              this.snackBar.open("Category " + category!.id + " has been updated.", undefined, { duration: 2000 });
+              this.dialogRef.close(true);
             },
             error: (error) => console.error(error)
           });
@@ -93,7 +93,6 @@ export class CategoryEditComponent extends BaseFormComponent implements OnInit{
               console.log("Category " + result.id + " has been created.");
               // go back to categories view
               this.dialogRef.close(true);
-              this.router.navigate(['/categories']);
             },
             error: (error) => console.error(error)
           });
