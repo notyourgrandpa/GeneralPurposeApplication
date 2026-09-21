6fafc3f547d9c04513e551f695d51a4aa8593681
Piolo Macayan <notmyvegetable@gmail.com>
2026-09-21 11:05:38 +0800

    change(settings): Add settings componetn

--- generalpurposeapplication.client/src/app/app.module.ts
+++ generalpurposeapplication.client/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ProductListComponent } from './products/components/product-list/product
 import { ProductListDialogComponent } from './products/components/product-list-dialog/product-list-dialog.component';
 import { InventoryLogDetailsComponent } from './inventory-logs/components/inventory-log-details/inventory-log-details.component';
 import { ProductEditDialogComponent } from './products/components/product-edit-dialog/product-edit-dialog.component';
+import { SettingsComponent } from './settings/settings/settings.component';
 
 
 @NgModule({
@@ -59,7 +60,8 @@ import { ProductEditDialogComponent } from './products/components/product-edit-d
     ExpenseEditComponent,
     ProductListDialogComponent,
     InventoryLogDetailsComponent,
-    ProductEditDialogComponent
+    ProductEditDialogComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
